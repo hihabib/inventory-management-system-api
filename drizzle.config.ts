298b4b9e9cec61a075cf407ff05c8b00d72873a7
Habibul Islam <habibulislam6862@gmail.com>
2025-08-26 11:11:43 +0600

    initial setup is ready

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+// drizzle.config.ts
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+    schema: './src/api/v1/drizzle/schema',
+    out: './src/api/v1/drizzle/migrations',
+    dialect: 'postgresql',
+    dbCredentials: {
+        url: process.env.DATABASE_URL as string,
+    },
+});
