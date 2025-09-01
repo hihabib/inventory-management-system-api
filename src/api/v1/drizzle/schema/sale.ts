72edfbeab0a6d3669cc35e61276f4680b7866b22
Habibul Islam <habibulislam6862@gmail.com>
2025-09-01 12:54:04 +0600

    updated

--- src/api/v1/drizzle/schema/sale.ts
+++ src/api/v1/drizzle/schema/sale.ts
@@ -11,7 +11,7 @@ import { outlets } from './outet';
 export const soldRecords = pgTable('sold_records', {
     id: uuid('id').defaultRandom().primaryKey(),
     userId: uuid('user_id').references(() => users.id, { onDelete: 'cascade' }).notNull(),
-    outletId: uuid('outlet_id').references(() => outlets.id, { onDelete: 'cascade' }).notNull(),
+    outletId: uuid('outlet_id').references(() => outlets.id, { onDelete: 'cascade' }),
     customerCategoryId: uuid('customer_category_id').references(() => customerCategories.id, { onDelete: 'restrict' }).notNull(),
     customerId: uuid('customer_id').references(() => customers.id, { onDelete: 'set null' }),
     totalQuantity: numeric('total_quantity', { precision: 10, scale: 2, mode: 'number' }).notNull(),
@@ -26,13 +26,13 @@ export const soldRecords = pgTable('sold_records', {
 export const soldItems = pgTable('sold_items', {
     id: uuid('id').defaultRandom().primaryKey(),
     soldRecordId: uuid('sold_record_id').references(() => soldRecords.id, { onDelete: 'cascade' }).notNull(),
-    inventoryItemId: uuid('inventory_item_id').references(() => inventoryItems.id, { onDelete: 'restrict' }).notNull(),
+    inventoryItemId: uuid('inventory_item_id').references(() => inventoryItems.id, { onDelete: 'restrict' }),
     productName: varchar('product_name', { length: 255 }).notNull(),
     discount: numeric('discount', { precision: 10, scale: 2, mode: 'number' }).notNull(),
     discountType: varchar('discount_type', { length: 50 }).notNull(), // 'fixed' or 'percentage'
     price: numeric('price', { precision: 10, scale: 2, mode: 'number' }).notNull(),
     quantity: numeric('quantity', { precision: 10, scale: 2, mode: 'number' }).notNull(),
-    unitSuffix: varchar('unit_suffix', { length: 50 }).notNull(),
+    unitSuffix: varchar('unit_suffix', { length: 50 }),
     stock: numeric('stock', { precision: 10, scale: 2, mode: 'number' }).notNull(),
 });
 
